3c3b8f77060c7976ae4618c8db252162f0c92f7b
Zoey Ling <zl64@rice.edu>
2021-03-09 22:15:58 -0600

    create window struct

--- project2/sendfile.c
+++ project2/sendfile.c
@@ -1,5 +1,3 @@
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -11,81 +9,103 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-/* A linked list node data structure to maintain application
-   information related to a connected socket */
+#define DATASIZE = 1024
+#define PACKETSIZE = 1034 // TODO
+#define BUFSIZE = 1024000
+#define WINDOWSIZE = 20
+#define SEQNUM = 40
+
+/* A sliding window structure */
 struct window
 {
-    char *buffer;
-
-
-    int socket;
-    struct sockaddr_in client_addr;
-    int pending_data; /* flag to indicate whether there is more data to send */
-    /* you will need to introduce some variables here to record
-       all the information regarding this socket.
-       e.g. what data needs to be sent next */
-    char *buf;
-    int idx;
-    int buf_len;
-    int testByte;
-    struct timeval *server_recv_start;
-    struct timeval *server_recv_end;
-    struct node *next;
+    bool ACK;
+    struct timeval *recv_start;
+    struct timeval *recv_end;
 };
 
 int main(int argc, char **argv) {
 
-        /*
-         * Parse the information about the receiver
-         */
-        char *receiver_info = argv(2);
-        char *recv_host;
-        char *recv_port;
-        recv_host = strtok(receiver_info, ':');
-        recv_port = atoi(strtok(NULL, ':'));
+    /*
+     * Parse the information about the receiver
+     */
+    char *receiver_info = argv(2);
+    char *recv_host;
+    int recv_port;
+    recv_host = strtok(receiver_info, ':');
+    recv_port = atoi(strtok(NULL, ':'));
 
-        /*
-         * Parse the information about the file to be sent
-         */
-        char *file_info = argv(4);
-        char *subdir;
-        char *filename;
-        subdir = strtok(file_info, '/');
-        filename = atoi(strtok(NULL, '/'));
+    /*
+     * Parse the information about the file to be sent
+     */
+    char *file_info = argv(4);
+    char *subdir;
+    char *filename;
+    subdir = strtok(file_info, '/');
+    filename = strtok(NULL, '/');
 
 
-        /* create the sender socket */
-        int send_sock;
-        if ((send_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-                perror("opening TCP socket");
-                abort();
+    /* create the sender socket */
+    int send_sock;
+    if ((send_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("opening TCP socket");
+            abort();
+    }
+
+    /*
+     * Put information about the receiver in a struct.
+     * Used in sendto and receiverfrom
+     */
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr(recv_host);
+    sin.sin_port = htons(recv_port);
+
+    struct sockaddr_in c_sin;
+    memset(&c_sin, 0, sizeof(c_sin));
+    c_sin.sin_family = AF_INET;
+    // TODO not sure
+    c_sin.sin_addr.s_addr = INADDR_ANY;
+    c_sin.sin_port = htons(0);
+    if (bind(send_sock, (const struct sockaddr *)&c_sin, sizeof(c_sin)) < 0) {
+        return -1;
+    }
+
+    struct window windows[WINDOWSIZE];
+
+    /*
+     * Open the file
+     */
+    FILE *fp = NULL;
+    fp = fopen(file_info, 'r');
+    size_t buf_size = 0
+    char buf[DATASIZE];
+    if (fp != null) {
+        fseek(fp, 0, SEEK_END);
+        int length = (int)ftell(fp);
+        rewind(fp);
+        int i = 0;
+        while (i < length) {
+            buf_size = fread(buf, sizeof(char), BUFSIZE, fp);
+            int sequence_count = buf_size / DATASIZE;
+            if (buf_size % BUFSIZE != 0)
+                sequence_count++;
+            int idx = 0;
+//            for
+
+            i += buf_size;
         }
+    } else {
+        perror("Error reading the file\n");
+    }
 
-        /*
-         * Put information about the receiver in a struct.
-         * Used in sendto and receiverfrom
-         */
-        struct sockaddr_in sin;
-        memset(&sin, 0, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = inet_addr(recv__host);
-        sin.sin_port = htons(recv_port);
 
-        /*
-         * Open the file
-         */
-        FILE *fp = NULL;
-        fp = fopen(file_info, 'r');
-
-        // Not sure.
-        int overhead;
-
-        /*
-         * Initialize packet
-         */
-        char *packet;
-        packet = malloc((35000+overhead)*sizeof(char));
-        char *buffer;
+    /*
+     * Initialize packet
+     */
+    char *packet;
+    packet = malloc((35000+overhead)*sizeof(char));
+    char *buffer;
 
 
 }
